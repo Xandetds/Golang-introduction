dc2e04cea8c7e2f581dc1a66cf677e5c2e35d12d
Alexandre Tibes <alexandretibes9@gmail.com>
2026-03-29 19:56:04 -0300

    feat: Adds channel and  goroutines exercises

---
+++ goroutines/channelSite.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func returnType(url string, out chan string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		out <- fmt.Sprintf("%s -> %s\n", url, err)
+		return
+	}
+
+	defer resp.Body.Close()
+	ctype := resp.Header.Get("content-type")
+	fmt.Printf("%s -> %s\n", url, ctype)
+}
+
+
+func main() {
+	urls := []string{
+		"https://golang.org",
+		"https://api.github.com",
+		"https://httpbin.org/ip",
+	}
+
+	ch := make(chan string)
+	for _, url := range urls {
+		go returnType(url, ch)
+	}
+
+	for range urls {
+		out := <-ch
+		fmt.Println(out)
+	}
+}
