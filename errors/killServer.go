218b2f48bf47811f9f20a4f92d683b2d8a254b24
Alexandre Tibes <alexandretibes9@gmail.com>
2026-03-29 18:44:30 -0300

    feat: adds all error treatment exercises

---
+++ errors/killServer.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+func killServer(pidFile string) error {
+
+	file, err := os.Open(pidFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	var pid int
+
+	if _, err := fmt.Fscanf(file, "%d", &pid); err != nil {
+		return errors.Wrap(err, "bad process id")
+	}
+
+
+	fmt.Printf("killing server with pid=%d\n", pid)
+
+	if err := os.Remove(pidFile); err != nil {
+		log.Printf("Warning: can remove pid file - %s", err)
+	}
+
+	return nil
+
+}
+
+
+func main(){
+	if err := killServer("server.pid"); err != nil {
+		log.Fatalf("error: %s", err)
+	}
+}
